692d96974adb75ce1947293e92532c24190d40c9
Yash Walia <yashwalia@Yashs-MacBook-Air.local>
2021-03-08 12:40:17 +0530

    add input functionality

--- Noobchain.java
+++ Noobchain.java
@@ -1,15 +1,12 @@
-import java.util.Date;
-import java.security.MessageDigest;
-import java.util.ArrayList;
+import java.util.*;
+import java.security.*;
 
 class StringUtil {
-	// Applies Sha256 to a string and returns the result.
 	public static String applySha256(String input) {
 		try {
 			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-			// Applies sha256 to our input,
 			byte[] hash = digest.digest(input.getBytes("UTF-8"));
-			StringBuffer hexString = new StringBuffer(); // This will contain hash as hexidecimal
+			StringBuffer hexString = new StringBuffer();
 			for (int i = 0; i < hash.length; i++) {
 				String hex = Integer.toHexString(0xff & hash[i]);
 				if (hex.length() == 1)
@@ -26,16 +23,28 @@ class StringUtil {
 class Block {
 	public String hash;
 	public String previousHash;
-	private String data; // our data will be a simple message.
-	private long timeStamp; // as number of milliseconds since 1/1/1970.
+	private String data; // A simple message.
+	private long timeStamp; // No of milliseconds since 1/1/1970.
 	private int nonce;
 
-	// Block Constructor.
+	// Block Constructor
 	public Block(String data, String previousHash) {
 		this.data = data;
 		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
 		this.hash = calculateHash(); // Making sure we do this after we set the other values.
+	}
+
+	public String getData() {
+		return this.data;
+	}
+
+	public long getTimeStamp() {
+		return this.timeStamp;
+	}
+
+	public int getNonce() {
+		return this.nonce;
 	}
 
 	// Calculate new hash based on blocks contents
@@ -51,25 +60,25 @@ class Block {
 			nonce++;
 			hash = calculateHash();
 		}
-		System.out.println("Block Mined!!! : " + hash);
+		System.out.println("Block Mined! -> " + hash);
 	}
 }
 
 class Noobchain {
 
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
-	public static int difficulty = 3;
+	public static int difficulty = 4;
 
 	public static Boolean isChainValid() {
 		Block currentBlock;
 		Block previousBlock;
 		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
-		// loop through blockchain to check hashes:
+		// Loop through blockchain to check hashes:
 		for (int i = 1; i < blockchain.size(); i++) {
 			currentBlock = blockchain.get(i);
 			previousBlock = blockchain.get(i - 1);
-			// compare registered hash and calculated hash:
+			// Compare registered hash and calculated hash:
 			if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
 				System.out.println("Current Hashes not equal");
 				return false;
@@ -89,36 +98,61 @@ class Noobchain {
 	}
 
 	public static void main(String[] args) {
-		// add our blocks to the blockchain ArrayList:
 
-		blockchain.add(new Block("Genesis Block", "0"));
-		System.out.println("Trying to Mine block 1... ");
-		blockchain.get(0).mineBlock(difficulty);
+		Scanner sc = new Scanner(System.in);
+		int ch, counter = 0;
+		String msg;
+		while (true) {
+			System.out.println("\n******** Blockchain in java ********");
+			System.out.println("1. Create Block\n2. View Blockchain\n3. Exit");
+			System.out.println("Enter your choice: ");
+			ch = sc.nextInt();
+			sc.nextLine();
 
-		blockchain.add(new Block("Second Block", blockchain.get(blockchain.size() - 1).hash));
-		System.out.println("Trying to Mine block 2... ");
-		blockchain.get(1).mineBlock(difficulty);
+			switch (ch) {
+			case 1:
+				System.out.print("\nEnter message: ");
+				msg = sc.nextLine();
+				if (counter == 0) {
+					blockchain.add(new Block(msg, "0"));
+					System.out.println("Trying to mine block " + counter + 1 + "...");
+					blockchain.get(counter).mineBlock(difficulty);
+				} else {
+					blockchain.add(new Block(msg, blockchain.get(blockchain.size() - 1).hash));
+					System.out.println("Trying to mine block " + counter + 1 + "...");
+					blockchain.get(counter).mineBlock(difficulty);
+				}
+				System.out.println("Blockchain is Valid: " + isChainValid());
+				counter += 1;
+				break;
+			case 2:
+				if (blockchain.isEmpty() == true) {
+					System.out.println("\nBlockchain empty!");
+				} else {
+					viewBlockchain();
+				}
+				break;
+			case 3:
+				System.exit(0);
+				break;
+			default:
+				System.out.println("\nError! Invalid\n");
+			}
+		}
+	}
 
-		blockchain.add(new Block("Third Block", blockchain.get(blockchain.size() - 1).hash));
-		System.out.println("Trying to Mine block 3... ");
-		blockchain.get(2).mineBlock(difficulty);
-
-		System.out.println("\nBlockchain is Valid: " + isChainValid());
-
+	static void viewBlockchain() {
 		System.out.println("\nThe block chain:");
 		System.out.println("[");
-
 		for (int i = 0; i < blockchain.size(); i++) {
 			System.out.println("\t{");
-			// System.out.println("\t " + blockchain.get(i));
 			System.out.println("\t  " + "'hash': " + "'" + blockchain.get(i).hash + "',");
 			System.out.println("\t  " + "'previousHash': " + "'" + blockchain.get(i).previousHash + "',");
-			// System.out.println("\t " + "'data': " + "'" + blockchain.get(i).data + "',");
-			// System.out.println("\t " + "'timeStamp': " + blockchain.get(i).timeStamp +
-			// ",");
-			// System.out.println("\t " + "'nonce': " + blockchain.get(i).nonce);
+			System.out.println("\t  " + "'data': " + "'" + blockchain.get(i).getData() + "',");
+			System.out.println("\t  " + "'timeStamp': " + blockchain.get(i).getTimeStamp() + ",");
+			System.out.println("\t  " + "'nonce': " + blockchain.get(i).getNonce());
 			System.out.println("\t},");
 		}
-		System.out.println("]\n");
+		System.out.println("]");
 	}
 }
--- tempCodeRunnerFile.java
+++
@@ -1,4 +0,0 @@
-String blockchainJson = new
-		// GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
-		// System.out.println("\nThe block chain: ");
-		// System.out.println(blockchainJson);
